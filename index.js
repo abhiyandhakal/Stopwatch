1bd0bd615472ceb4a001a6a61c631a11147987ae
Abhiyan-Dhakal <itsabhiyandhakal@gmail.com>
2022-01-15 15:48:06 +0545

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const btns_button = document.querySelectorAll('button');
 const btnStart_button = document.getElementById('btn-start');
 const btnPause_button = document.getElementById('btn-pause');
 const btnReset_button = document.getElementById('btn-reset');
@@ -11,6 +12,11 @@ let hour = 0;
 let minute = 0;
 let second = 0;
 
+// audio
+let audio = new Audio();
+audio.src = "sound/button-click.mp3";
+
+
 function writingTime(time, time_span) {
    if (time < 10) {
       time_span.innerHTML = '0' + time;
@@ -21,7 +27,6 @@ function writingTime(time, time_span) {
 }
 
 btnStart_button.addEventListener('click', () => {
-   
    stopwatch = setInterval(() => {
       second++;
       writingTime(second, second_span);
@@ -30,14 +35,15 @@ btnStart_button.addEventListener('click', () => {
          second = 0;
          minute++;
          writingTime(minute, minute_span);
-         }
-   
+      }
+      
       if (minute === 60) {
          minute = 0;
          hour++;
          writingTime(hour, hour_span);
       }
    }, 1000);
+
    btnPause_button.disabled = false;
    btnReset_button.disabled = false;
    btnStart_button.disabled = true;
@@ -55,12 +61,19 @@ btnReset_button.addEventListener('click', () => {
    hour_span.innerHTML = '00';
    minute_span.innerHTML = '00';
    second_span.innerHTML = '00';
-
+   
    hour = 0;
    minute = 0;
    second = 0;
+   clearInterval(stopwatch);
    
    btnStart_button.disabled = false;
    btnPause_button.disabled = false;
    btnReset_button.disabled = true;
+})
+
+btns_button.forEach(button => {
+   button.addEventListener('click', function () {
+      audio.play();
+   })
 })
